2fde752b1f67cf1503800a0762400362086958ac
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2025-10-15 20:37:00 +0300

    test gh pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://TutoriatPF.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/TutoriatPF2026',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TutoriatPF', // Usually your GitHub org/user name.
+  projectName: 'TutoriatPF2026', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
